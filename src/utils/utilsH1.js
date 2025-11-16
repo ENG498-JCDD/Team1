c9ef4ac4d10317c5f4fb62cb373b7d820b67f7c1
Nazifa Nuha Chowdhury <nnchowdh@ncsu.edu>
2025-11-16 12:15:01 -0500

    Update utilsH1.js

--- src/utils/utilsH1.js
+++ src/utils/utilsH1.js
@@ -1,3 +1,5 @@
+import {rollups} from "npm:d3-array";
+
 /** getUniquePropListBy()
  * Goal: Create a unique list of `x` property
  *       in an array of objects.
@@ -14,4 +16,78 @@ export const getUniquePropListBy = (arr, key) => {
     uniqList.push(o[key])
   }
   return uniqList
+}
+
+// oneLevelRollUpFlatMap function
+
+export const oneLevelRollUpFlatMap = (data, level1Key, countKey) => {
+  // 1. Rollups on one level
+  const colTotals = rollups(
+    data,
+    (v) => v.length, // Count length of leaf node
+    (d) => d[level1Key] // d["race"]
+  )
+  
+  // 2. Flatten back to array of objects
+  const flatTotals = colTotals.flatMap((e) => {
+    return {
+      [level1Key]: e[0],
+      [countKey]: e[1]
+    }
+  })
+  
+  // 3. Return the sorted totals
+  return flatTotals
+}
+
+// twoLevelRollUpFlatMap function
+
+// threeLevelRollUpFlatMap function
+
+export const threeLevelRollUpFlatMap = (data, level1Key, level2Key, level3Key, countKey) => {
+  
+  // 1. Rollups on 3 nested levels
+  const colTotals = rollups(
+    data,
+    (v) => v.length, 
+    (d) => d[level1Key], 
+      (d) => d[level2Key], 
+        (d) => d[level3Key] 
+  )
+  
+  // 2. Flatten 1st level
+  const flatTotals = colTotals.flatMap((l1Elem) => {
+    
+    // 2.1 Get level 1 value
+    let l1KeyValue = l1Elem[0]
+    
+    // 2.2 Flatten 2nd level
+    const flatLevel2 = l1Elem[1].flatMap((l2Elem) => {
+      
+      // 2.2.1 Get level 2 value
+      let l2KeyValue = l2Elem[0]
+      
+      // 2.3 Flatten 3rd level
+      const flatLevel3 = l2Elem[1].flatMap((l3Elem) => {
+        
+        // 2.3.1 Get level 3 value
+        let l3KeyValue = l3Elem[0]
+        
+        // 2.3.2 Return fully populated object
+        return {
+          [level1Key]: l1KeyValue,
+          [level2Key]: l2KeyValue,
+          [level3Key]: l3KeyValue,
+          [countKey]: l3Elem[1]
+        }
+      })
+      
+      return flatLevel3
+    })
+    
+    return flatLevel2
+  })
+  
+  // 3. Return the flattened array
+  return flatTotals
 }
