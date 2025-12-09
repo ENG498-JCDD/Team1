3a65f52b26495a8217a5128155d9762acd99f4d8
Nazifa Nuha Chowdhury <nnchowdh@ncsu.edu>
2025-12-08 20:14:04 -0500

    Update utilsH1.js

--- src/utils/utilsH1.js
+++ src/utils/utilsH1.js
@@ -1,5 +1,5 @@
-import {rollups} from "npm:d3-array";
-import {utcFormat} from "npm:d3-time-format";
+import {rollups, ascending} from "npm:d3-array";
+import {utcParse, utcFormat} from "npm:d3-time-format";
 
 /** getUniquePropListBy()
  * Goal: Create a unique list of `x` property
@@ -131,6 +131,48 @@ export const threeLevelRollUpFlatMap = (data, level1Key, level2Key, level3Key, c
   
   // 3. Return the flattened array
   return flatTotals
+}
+
+
+// Date parsers and formatters for traffic stops data
+const parseDate = utcParse("%Y-%m-%dT%H:%MZ");
+const formatYearNumber = utcFormat("%Y");
+const formatMonthNumber = utcFormat("%m");
+const formatWeekNumber = utcFormat("%U");
+
+export const mapDateObjectForStops = (data, dateString) => {
+  
+  // Use .map() to iterate the data and create new date properties
+  const updatedData = data.map((stop) => {
+    
+    // Create dynamic keys for new properties
+    const objField = dateString + "_obj"
+    const weekField = dateString + "_week"
+    const monthField = dateString + "_month"
+    const yearField = dateString + "_year"
+    
+    // Skip any null datetime values
+    if (stop[dateString] != null) {
+      
+      const dateObj = stop[dateString]
+      
+      // Assign the Date object
+      stop[objField] = dateObj
+      
+      // Extract and assign year, month, week as numbers
+      stop[yearField] = Number(formatYearNumber(dateObj))
+      stop[monthField] = Number(formatMonthNumber(dateObj))
+      stop[weekField] = Number(formatWeekNumber(dateObj))
+    }
+    
+    return stop
+  })
+  
+  const sortedData = updatedData.sort((a, b) => {
+    return ascending(a[dateString + "_obj"], b[dateString + "_obj"])
+  })
+  
+  return sortedData
 }
 
 export const normalizeLocation = (d) => {
